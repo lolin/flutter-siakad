9d2383e4e7bc7ca8d21c2bd2a8957887a8f46a86
lolin <putu.lolin@gmail.com>
2023-11-07 13:42:46 +0700

    finalisasi

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,13 @@ import 'package:siakad_indra/bloc/schedule/schedule_bloc.dart';
 import 'package:siakad_indra/data/datasources/auth_local_datasources.dart';
 import 'package:siakad_indra/pages/auth/auth_page.dart';
 import 'package:siakad_indra/pages/mahasiswa/mahasiswa_page.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'bloc/khs/khs_bloc.dart';
 
 void main() {
+  debugPrint('print 1');
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -17,6 +20,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    debugPrint('print 2');
+
     return MultiBlocProvider(
       providers: [
         BlocProvider(
@@ -37,18 +42,26 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home: FutureBuilder<bool>(
-            future: AuthLocalDatasource().isLogin(),
-            builder: (context, snapshot) {
+          future: AuthLocalDatasource().isLogin(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
               if (snapshot.hasData && snapshot.data!) {
+                debugPrint('print 4');
+                FlutterNativeSplash.remove();
                 return BlocProvider(
                   create: (context) => KhsBloc(),
                   child: const MahasiswaPage(),
                 );
               } else {
+                debugPrint('print 3');
+                FlutterNativeSplash.remove();
                 return const AuthPage();
               }
-              // return const SplashPage();
-            }),
+            }
+            return const SizedBox();
+            // return const SplashPage();
+          },
+        ),
       ),
     );
   }
